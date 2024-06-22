efc37a016292f73bbf55f0abe0f68c4ca1c5a283
fallbean <fallbean.com@gmail.com>
2024-06-22 10:46:21 +0800

    feat: raid

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      color: {
+        md: {
+          secondary: "#121212",
+          red: '#ef4444',
+          blue: '#3b82f6',
+          amber: "#f59e0b",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+

cff0654d5ad4e7da251f61aaa16bc3dcf6101b42
fallbean <fallbean.com@gmail.com>
2024-06-22 21:21:48 +0800

    feat: raid valtan

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,6 @@ module.exports = {
   theme: {
     extend: {
       color: {
-        md: {
-          secondary: "#121212",
-          red: '#ef4444',
-          blue: '#3b82f6',
-          amber: "#f59e0b",
-        }
       }
     },
   },
